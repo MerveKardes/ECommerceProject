265e6efee9cc818e757b4c37f693de2f79e681e4
mmervekardess <mmervekardess@gmail.com>
2020-12-16 20:46:05 +0300

    Katmanlar oluşturuldu

---
+++ Entities/Kategori.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.Entities
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Ad{ get; set; }
+
+        public List<UrunKategori> UrunKategoriler { get; set; }
+    }
+}
---
+++ Entities/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.Entities
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Resim { get; set; }
+        public decimal Fiyat { get; set; }
+        public List<UrunKategori> UrunKategoriler { get; set; }
+
+    }
+}
