558942777f109a039557c03b4284f3e85ad5a25e
mmervekardess <mmervekardess@gmail.com>
2020-12-18 13:35:22 +0300

    ViewComponents eklendi

---
+++ ViewComponents/KategoriList.cs
@@ -0,0 +1,23 @@
+﻿using E_Ticaret.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.ViewComponents
+{
+    public class KategoriList :ViewComponent
+    {
+       private readonly IKategoriRepository _kategoriRepository;
+        public KategoriList(IKategoriRepository kategoriRepository)
+        {
+            _kategoriRepository = kategoriRepository;
+        }
+       
+        public IViewComponentResult Invoke()
+        {
+            return View(_kategoriRepository.GetirHepsi());
+        }
+    }
+}
---
+++ ViewComponents/UrunList.cs
@@ -0,0 +1,22 @@
+﻿using E_Ticaret.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.ViewComponents
+{
+    public class UrunList: ViewComponent
+    {
+        private readonly IUrunRepository _urunRepository;
+        public UrunList(IUrunRepository urunRepository)
+        {
+            _urunRepository = urunRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_urunRepository.GetirHepsi());
+        }
+    }
+}
