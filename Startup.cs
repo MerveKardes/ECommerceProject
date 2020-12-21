00d9c013d0c195f508d6091d3eeb85e72eeccd44
mmervekardess <mmervekardess@gmail.com>
2020-12-21 12:48:10 +0300

    Identity Initializer

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,15 @@ namespace E_Ticaret
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Context>();
-            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<Context>();
+            services.AddIdentity<AppUser, IdentityRole>(opt=>
+            {
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                
+            }).AddEntityFrameworkStores<Context>();
             services.AddScoped<IKategoriRepository, KategoriRepository>();
             services.AddScoped<IUrunRepository, UrunRepository>();
             services.AddScoped<IUrunKategoriRepository, UrunKategoriRepository>();
@@ -37,7 +45,8 @@ namespace E_Ticaret
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment 
+            env,UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -49,6 +58,10 @@ namespace E_Ticaret
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            IdentityInitializer.OlusturAdmin(userManager, roleManager);
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
