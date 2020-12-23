0aa304316b6c486bc08cca9d3e705c3328fd59e5
mmervekardess <mmervekardess@gmail.com>
2020-12-23 13:09:42 +0300

    Resim ekleme

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Ticaret.Entities;
 using E_Ticaret.Interfaces;
 using E_Ticaret.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -29,6 +31,34 @@ namespace E_Ticaret.Areas.Admin.Controllers
         public IActionResult Ekle()
         {
             return View(new UrunEkleModel());
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(UrunEkleModel model)
+        {
+            Urun urun = new Urun();
+            if (ModelState.IsValid)
+            {
+                if (model.Resim != null)
+                {
+                    var uzanti = Path.GetExtension(model.Resim.FileName);
+                    var yeniResimAd = Guid.NewGuid() + uzanti;
+                    var yuklenecekYer = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/"+yeniResimAd);
+
+                    var stream = new FileStream(yuklenecekYer, FileMode.Create);
+                    model.Resim.CopyTo(stream);
+
+                    urun.Resim = yeniResimAd;
+                }
+               
+                urun.Ad = model.Ad;
+                urun.Fiyat = model.Fiyat;
+
+                _urunRepository.Ekle(urun);
+
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            return View(model);
         }
     }
 }
